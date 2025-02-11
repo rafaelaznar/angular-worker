5b501ff5c8d9e413e1d84e2a6388bc70d44b4e96
Rafael Aznar <rafaaznar@gmail.com>
2025-02-11 19:47:43 +0100

    2 workers and interactivity buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,8 +32,28 @@ export class AppComponent {
     }
   }
 
+  on_init_worker2() {
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker(new URL('./app2.worker', import.meta.url));
+
+      worker.onmessage = ({ data }) => {
+        console.log(`page got message: ${data}`);
+      };
+
+      worker.postMessage('hello');
+
+    } else {
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+  }
+
   on_saluda() {
-    console.log('saluda');
+    console.log('saluda in console!!!');
+  }
+  on_saluda2() {
+    alert('saluda in alert!!!');
   }
 }
 
