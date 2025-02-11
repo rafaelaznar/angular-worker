84dc03112b75c1be0f4df102ee02bd1c2e305d67
Rafael Aznar <rafaaznar@gmail.com>
2025-02-11 19:37:01 +0100

    inicio worker1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,35 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'angular-worker';
+
+  constructor() {
+  }
+
+  on_init_worker() {
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker(new URL('./app.worker', import.meta.url));
+
+      worker.onmessage = ({ data }) => {
+        console.log(`page got message: ${data}`);
+      };
+
+      worker.postMessage('hello');
+
+    } else {
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+  }
+
+  on_saluda() {
+    console.log('saluda');
+  }
 }
+
+
+
+
---
+++ src/app/app.worker.ts
@@ -0,0 +1,30 @@
+/// <reference lib="webworker" />
+
+function fibonacci(n: number): number {
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function procesoLargo() {
+  let cont = 0;
+  for (let i = 0; i < 40; i++) {
+    cont = cont + (i % 5) + (i % 2);
+    // add fibonacci to cont
+    cont = cont + fibonacci(i);
+    // add random int to cont
+    cont = cont + Math.floor(Math.random() * 5);
+  }
+  return cont;
+
+}
+
+addEventListener('message', ({ data }) => {
+  for (let i = 0; i < 10; i++) {
+    postMessage(`worker response to ${data} ${procesoLargo()}`);
+  }
+
+});
+
+
